0afb933acb7fb928bc833778153e4df4b159c8d6
SeanWahl <seanwahl19@gmail.com>
2023-01-24 11:09:31 -0800

    lab 2 in class work

--- Lab 2/src/encoder_reader.py
+++ Lab 2/src/encoder_reader.py
@@ -7,5 +7,42 @@
 @date
 """
 
+import time
+
 class encoder:
     """@brief"""
+    
+    def __init__(self, pin1, pin2, timer):
+        self.pin1 = pyb.Pin(pin1, pyb.Pin.IN)
+        self.pin2 = pyb.Pin(pin2, pyb.Pin.IN)
+        self.timer = pyb.Timer (timer, period = 0xFFFF, prescaler = 0)
+        self.ch1 = self.timer.channel(1, pyb.Timer.ENC_AB, pin=self.pin1)
+        self.ch2 = self.timer.channel(2, pyb.Timer.ENC_AB, pin=self.pin2)
+        self.count = 0
+        self.prev = 0
+        
+    def read_encoder(self):
+        self.current = self.timer.counter()
+        self.delta = self.current-self.prev
+        if abs(self.delta) > 0xFFFF/2:
+            if self.delta > 0:
+                self.count += self.delta - 0xFFFF
+            else:
+                self.count += self.delta + 0xFFFF
+        else:
+            self.count += self.delta
+            
+        self.prev = self.current
+        return self.count
+        
+    def zero(self):
+        self.count = 0
+        
+if __name__ == "__main__":
+    my_encoder = encoder(pyb.Pin.board.PC6, pyb.Pin.board.PC7, 8)
+    while True:
+        for n in range(10):
+            print(my_encoder.read_encoder())
+            if n == 9:
+                my_encoder.zero()
+            time.sleep(0.5)
--- Lab 2/src/motor_driver.py
+++ Lab 2/src/motor_driver.py
@@ -8,4 +8,35 @@
 """
 
 class MotorDriver:
-    """
+    """@brief"""
+    
+    def __init__(self, enPin, pin1, pin2, timer):
+        self.pin1 = pyb.Pin(pin1, pyb.Pin.IN)
+        self.pin2 = pyb.Pin(pin2, pyb.Pin.IN)
+        self.enPin = pyb.Pin(enPin, pyb.Pin.OUT_PP)
+        self.timer = pyb.Timer(timer, freq = 20000)
+        self.ch1 = self.timer.channel(1, pyb.Timer.PWM, pin=self.pin1)
+        self.ch2 = self.timer.channel(2, pyb.Timer.PWM, pin=self.pin2)
+        self.ch1.pulse_width_percent(0)
+        self.ch2.pulse_width_percent(0)
+        self.enPin.high()
+        
+    def set_duty_cycle(self, percent):
+        if percent > 100:
+            percent = 100
+        elif percent < -100:
+            percent = -100
+        
+        if percent > 0:
+            self.ch2.pulse_width_percent(0)
+            self.ch1.pulse_width_percent(percent)
+            
+        else:
+            self.ch1.pulse_width_percent(0)
+            self.ch2.pulse_width_percent(percent)
+                
+        
+        
+if __name__ == "__main__":
+    my_motor = MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
+    my_motor.set_duty_cycle(100)
